72339d22a7e0e9238ea481c90c11e03885eb52e1
Thomas Hybel <tehybel@gmail.com>
2019-01-31 15:03:53 +0100

    tmp

---
+++ trees.js
@@ -0,0 +1,10 @@
+function plant_tree () {
+	var species = document.getElementById("species").value;
+	var height = document.getElementById("height").value;
+	var name = document.getElementById("name").value;
+
+	console.log(species);
+	console.log(height);
+	console.log(name);
+
+}
