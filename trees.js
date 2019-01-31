8a716e84c8e659ba3dfc6465741595d23edb6b02
Thomas Hybel <tehybel@gmail.com>
2019-01-31 15:06:34 +0100

    tmp

--- trees.js
+++ trees.js
@@ -5,6 +5,11 @@ function save_tree (tree) {
 	localStorage.setItem("trees", trees);
 }
 
+function get_trees () {
+	var trees = localStorage.getItem("trees");
+	return trees;
+}
+
 // Retrieve
 document.getElementById("result").innerHTML = ;
 
@@ -14,10 +19,15 @@ function plant_tree () {
 	var height = document.getElementById("height").value;
 	var name = document.getElementById("name").value;
 
+	var tree = {
+		"species": species,
+		"height": height,
+		"name": name
+	};
 
-	console.log(species);
-	console.log(height);
-	console.log(name);
+	save_tree(tree);
+
+	console.log(get_trees());
 
 }
 
